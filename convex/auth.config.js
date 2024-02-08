fb7cf8eae05a4c234d9ac3d5a438cf9ba80cdbdb
Ramy <nguyentunglam724@gmail.com>
2024-02-08 23:13:22 +0700

    init commit

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://sacred-scorpion-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+// ! means never undefined
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvextClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  // const { user, isLoading } = useAuth();
+  // if (isLoading) {
+  //   return <AuthLoading />;
+  // }
+  // if (!user) {
+  //   return <Authenticated />;
+  // }
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
