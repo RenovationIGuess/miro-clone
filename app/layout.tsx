59f409c7b675e6fb6a16c97cb503bebe0fb891fc
Ramy <nguyentunglam724@gmail.com>
2024-02-13 11:54:21 +0700

    feat: init create mutation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ConvextClientProvider } from "@/providers/convex-client-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvextClientProvider>{children}</ConvextClientProvider>
+        <ConvextClientProvider>
+          {children}
+          <Toaster />
+        </ConvextClientProvider>
       </body>
     </html>
   );
