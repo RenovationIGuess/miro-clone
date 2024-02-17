2c599d32f895c3d064fae1d4ea5c1b46905e533f
Ramy <nguyentunglam724@gmail.com>
2024-02-17 16:36:05 +0700

    feat: init canvas layout

---
+++ app/board/[boardId]/_components/canvas.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Info } from "./info";
+import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
+
+interface CanvasProps {
+  boardId: string;
+}
+
+export const Canvas = ({ boardId }: CanvasProps) => {
+  return (
+    <div>
+      <Info />
+      <Participants />
+      <Toolbar />
+    </div>
+  );
+};
