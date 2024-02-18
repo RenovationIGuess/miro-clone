81cb17c28e3df7f6d34f250551d23fd20a7b9f9b
Ramy <nguyentunglam724@gmail.com>
2024-02-18 17:13:12 +0700

    feat: add room participants

---
+++ app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,32 @@
+import { Hint } from "@/components/hint";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface UserAvatarProps {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  borderColor?: string;
+}
+
+export const UserAvatar = ({
+  src,
+  name,
+  fallback,
+  borderColor,
+}: UserAvatarProps) => {
+  return (
+    <Hint label={name || "Guest"} side="bottom" sideOffset={18}>
+      <Avatar
+        style={{
+          borderColor,
+        }}
+        className="h-8 w-8 border-2"
+      >
+        <AvatarImage src={src} alt={name} />
+        <AvatarFallback className="text-xs font-semibold">
+          {fallback}
+        </AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
+};
