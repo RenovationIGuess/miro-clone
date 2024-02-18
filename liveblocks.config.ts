866c1063a292f698b4c0536dfdb85e5b8d9580f7
Ramy <nguyentunglam724@gmail.com>
2024-02-18 12:37:26 +0700

    feat: add romm auth

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,9 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_2cHq2gkhzt0vKrj5k7V9uOPBE45GJPZ3LzITXpR5FqFYDOcxiS1FBJN_EXFE4gia",
+  // publicApiKey:
+  //   "pk_dev_2cHq2gkhzt0vKrj5k7V9uOPBE45GJPZ3LzITXpR5FqFYDOcxiS1FBJN_EXFE4gia",
+  authEndpoint: "/api/liveblocks-auth",
 });
 
 // Presence represents the properties that exist on every user in the Room
@@ -27,8 +28,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  };
 };
 
 // Optionally, the type of custom events broadcast and listened to in this
